fd06d9bd48f82c601f4a65467d795baf6b476f1b
Ari Cummings <ari@aricummings.com>
2025-10-16 14:32:14 -0700

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
+    cors: {
+      origin: "https://points.delphigamerz.xyz", // Allow access from this origin
+      methods: ['GET', 'POST'], // Specify allowed methods
+      allowedHeaders: ['Content-Type'], // Specify allowed headers
+    },
+    allowedHosts: [
+      'points.delphigamerz.xyz', // Add the host to the allowed hosts
+      'delphigamerz.xyz',
+      'aricummings.com',
+      'points.aricummings.com',
+    ],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === 'development',
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
